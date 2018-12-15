fccfe3e715d14dbe6573b68f6fb982cdb8cea73f
Brad Niedt <bradleyniedt@gmail.com>
2018-12-15 15:16:59 -0700

    adds user validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,19 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "relationships" do
+  describe 'relationships' do
     it { should have_many(:orders)}
     it { should have_many(:items)}
   end
-
-  
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:street)}
+    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:state)}
+    it { should validate_presence_of(:zip)}
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:password)}
+    it { should validate_presence_of(:role)}
+    it { should validate_presence_of(:enabled)}
+  end
 end
