c44adcc8ffc6b30e915413b17ffa03239a6e24f6
Brad Niedt <bradleyniedt@gmail.com>
2018-12-15 15:22:53 -0700

    adds item validation test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,8 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe "relationships" do
+  describe 'relationships' do
     it { should belong_to(:user)}
     it { should have_many(:orders).through(:order_items)}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:image_link)}
+    it { should validate_presence_of(:inventory)}
+    it { should validate_presence_of(:description)}
+    it { should validate_presence_of(:current_price)}
+    it { should validate_presence_of(:enabled)}
+    it { should validate_presence_of(:user_id)}
   end
 end
