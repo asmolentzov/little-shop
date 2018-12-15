39b2c11a724e193350b1eba735e7ec7e1d6442b6
William Petty <william@snakeweed.com>
2018-12-15 14:27:54 -0700

    Initial commit

---
+++ db/migrate/20181215211701_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :image_link
+      t.string :inventory
+      t.string :description
+      t.integer :current_price
+      t.boolean :enabled
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
