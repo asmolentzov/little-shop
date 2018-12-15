dd32e1e821a64337a119a8bc1337320676a7b2f4
Brad Niedt <bradleyniedt@gmail.com>
2018-12-15 15:20:19 -0700

    adds test for order validations

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,8 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  describe "relationships" do
+  describe 'relationships' do
     it { should belong_to(:user)}
     it { should have_many(:items).through(:order_items)}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:status)}
+    it { should validate_presence_of(:user_id)}
+  end
+
 end
