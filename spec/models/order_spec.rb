6861aa50c4360bde14d8e70df8edd65b177a64ce
Brad Niedt <bradleyniedt@gmail.com>
2018-12-15 16:07:49 -0700

    adds relationhship test for order model for having many order items

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   describe 'relationships' do
     it { should belong_to(:user)}
+    it { should have_many(:order_items)}
     it { should have_many(:items).through(:order_items)}
   end
 
