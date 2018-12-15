b1dc865a3ca33c1a6cd354ab35e91cfe438d04d8
Brad Niedt <bradleyniedt@gmail.com>
2018-12-15 16:05:44 -0700

    adds validation for item having order items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to(:user)}
+    it { should have_many(:order_items)}
     it { should have_many(:orders).through(:order_items)}
   end
 
