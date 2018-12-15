0fe5529834712e7c9076d5176b9748220502b372
Brad Niedt <bradleyniedt@gmail.com>
2018-12-15 15:24:34 -0700

    implements item validations to pass item validation test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  validates_presence_of :name, :image_link, :inventory, :description,
+                        :current_price, :enabled, :user_id
+
   belongs_to :user
 
   has_many :order_items
