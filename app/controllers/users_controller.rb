aaf7256a3256409f011069ecbdfc9358268f484f
William Petty <william@snakeweed.com>
2018-12-17 15:51:41 -0700

    Basic display on User Show Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   def index
-    
+
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
-    
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root 'welcome#index'
-  
+
   get '/cart', to: "carts#show"
   get '/login', to: "sessions#new"
-  get '/register', as: :registration, to: "users#new" 
+  get '/register', as: :registration, to: "users#new"
 
   resources :items, only: [:index]
   resources :users, as: :merchants, only: [:index]
+  resources :users, only: [:show, :update]
 end
