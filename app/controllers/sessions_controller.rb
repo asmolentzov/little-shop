d06f7d63cee6dab007ff98aa87ce138a43429af5
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-18 15:43:21 -0700

    Adds route and controller method for logging in and creating a new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,16 @@ class SessionsController < ApplicationController
   def new
 
   end
+  
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
 
   def destroy
     redirect_to root_path
