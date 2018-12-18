a00ed0f2593cff4a776b706b4dda655a8f738714
Brad Niedt <bradleyniedt@gmail.com>
2018-12-17 17:18:53 -0700

    Added allow any instance of stub to user show page test and implemented current user helper method in user show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    
   end
 
   def new
 
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to profile_path
+    else
+      render :new
+    end
   end
 
   private
--- spec/features/user_show_page_spec.rb
+++ spec/features/user_show_page_spec.rb
@@ -6,6 +6,8 @@ describe 'USER SHOW PAGE' do
       user_1 = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1',
         zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 0, enabled: true)
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
       visit profile_path
 
       expect(page).to have_content(user_1.name)
