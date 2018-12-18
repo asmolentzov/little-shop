7ee1158f09b2981c14c0d97f8f6723180e706f0b
Brad Niedt <bradleyniedt@gmail.com>
2018-12-18 16:47:13 -0700

    Adds tests for all user types being redirected if already logged in.

--- spec/features/visitor_can_login_spec.rb
+++ spec/features/visitor_can_login_spec.rb
@@ -5,13 +5,13 @@ describe 'As a visitor to the site' do
     context 'as a default user' do
       it 'should let me log in and send me to my profile page' do
         user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1')
-        
+
         visit login_path
-        
+
         fill_in :email, with: user.email
         fill_in :password, with: user.password
         click_button 'Log In'
-        
+
         expect(current_path).to eq(profile_path)
         expect(page).to have_content("Logged In Successfully")
       end
@@ -19,13 +19,13 @@ describe 'As a visitor to the site' do
     context 'as a merchant user' do
       it 'should let me log in and send me to my dashboard page' do
         user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 1)
-        
+
         visit login_path
-        
+
         fill_in :email, with: user.email
         fill_in :password, with: user.password
         click_button 'Log In'
-        
+
         expect(current_path).to eq(dashboard_path)
         expect(page).to have_content("Logged In Successfully")
       end
@@ -33,15 +33,51 @@ describe 'As a visitor to the site' do
     context 'as an admin user' do
       it 'should let me log in and send me to the home page' do
         user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 2)
-        
+
         visit login_path
-        
+
         fill_in :email, with: user.email
         fill_in :password, with: user.password
         click_button 'Log In'
-        
+
         expect(current_path).to eq(root_path)
         expect(page).to have_content("Logged In Successfully")
+      end
+    end
+    context 'as an already-logged-in default user' do
+      it 'should redirect me to my profile page' do
+        user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1')
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit login_path
+
+        expect(current_path).to eq(profile_path)
+        expect(page).to have_content("You are already logged in")
+      end
+    end
+    context 'as an already-logged-in merchant user' do
+      it 'should redirect me to my profile page' do
+        user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 1)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit login_path
+
+        expect(current_path).to eq(dashboard_path)
+        expect(page).to have_content("You are already logged in")
+      end
+    end
+    context 'as an already-logged-in admin user' do
+      it 'should redirect me to my profile page' do
+        user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 2)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit login_path
+
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content("You are already logged in")
       end
     end
   end
