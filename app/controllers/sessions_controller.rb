76dbc978d4d39513793e6004f2f38dde517da153
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-18 15:46:11 -0700

    Adds merchant and admin paths to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,13 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to profile_path
+      if user.default?
+        redirect_to profile_path
+      elsif user.merchant? 
+        redirect_to dashboard_path
+      elsif user.admin?
+        redirect_to root_path
+      end
     else
       render :new
     end
