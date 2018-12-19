eded474e10ca76d7017fb52696f077bfb1848a2b
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-19 16:27:58 -0700

    Adds before_action filter to admin users controller for requiring admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,14 @@
 class Admin::UsersController < ApplicationController
-  
+  before_action :require_admin
   def index
     
   end
   
+  private
+  
+  def require_admin
+    unless current_user
+      render file: "public/404", status: 404, layout: false
+    end
+  end
 end
