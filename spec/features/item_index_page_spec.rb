41a67cf81016c2ff7476b3b680836163fb935db1
Brad Niedt <bradleyniedt@gmail.com>
2018-12-19 15:48:39 -0700

    Creates test for seeing all items as any user type

---
+++ spec/features/item_index_page_spec.rb
@@ -0,0 +1,139 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  describe 'when I visit /items' do
+    context 'as a default user' do
+      it 'should see all enabled items and their information' do
+        user_1 = User.create(name: "user_1", password: "test", street: "street",
+          city: "city", state: "CO", zip: "80219", email: "email", role: 0, enabled: true)
+        user_2 = User.create(name: "user_1", password: "test", street: "street",
+          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
+        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+        inventory: 3, description: 'apple one', current_price: 200, enabled: true)
+        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        inventory: 4, description: 'apple two', current_price: 400, enabled: false)
+        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        inventory: 5, description: 'apple three', current_price: 500, enabled: true)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+        visit items_path
+
+        expect(page).to have_content(item_1.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/g/200/300']")
+        expect(page).to have_content(item_1.user.name)
+        expect(page).to have_content(item_1.inventory)
+        expect(page).to have_content(item_1.current_price)
+        expect(page).to_not have_content(item_2.name)
+        expect(page).to_not have_css("img[src*='https://picsum.photos/5472/3648?image=1083']")
+        expect(page).to_not have_content(item_2.user.name)
+        expect(page).to_not have_content(item_2.inventory)
+        expect(page).to_not have_content(item_2.current_price)
+        expect(page).to have_content(item_3.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/200/300?image=0']")
+        expect(page).to have_content(item_3.user.name)
+        expect(page).to have_content(item_3.inventory)
+        expect(page).to have_content(item_3.current_price)
+      end
+    end
+    context 'as a merchant user' do
+      it 'should see all enabled items and their information' do
+        user_1 = User.create(name: "user_1", password: "test", street: "street",
+          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
+        user_2 = User.create(name: "user_1", password: "test", street: "street",
+          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
+        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+        inventory: 3, description: 'apple one', current_price: 200, enabled: true)
+        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        inventory: 4, description: 'apple two', current_price: 400, enabled: false)
+        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        inventory: 5, description: 'apple three', current_price: 500, enabled: true)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+        visit items_path
+
+        expect(page).to have_content(item_1.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/g/200/300']")
+        expect(page).to have_content(item_1.user.name)
+        expect(page).to have_content(item_1.inventory)
+        expect(page).to have_content(item_1.current_price)
+        expect(page).to_not have_content(item_2.name)
+        expect(page).to_not have_css("img[src*='https://picsum.photos/5472/3648?image=1083']")
+        expect(page).to_not have_content(item_2.user.name)
+        expect(page).to_not have_content(item_2.inventory)
+        expect(page).to_not have_content(item_2.current_price)
+        expect(page).to have_content(item_3.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/200/300?image=0']")
+        expect(page).to have_content(item_3.user.name)
+        expect(page).to have_content(item_3.inventory)
+        expect(page).to have_content(item_3.current_price)
+      end
+    end
+    context 'as an admin user' do
+      it 'should see all enabled items and their information' do
+        user_1 = User.create(name: "user_1", password: "test", street: "street",
+          city: "city", state: "CO", zip: "80219", email: "email", role: 2, enabled: true)
+        user_2 = User.create(name: "user_1", password: "test", street: "street",
+          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
+        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+        inventory: 3, description: 'apple one', current_price: 200, enabled: true)
+        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        inventory: 4, description: 'apple two', current_price: 400, enabled: false)
+        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        inventory: 5, description: 'apple three', current_price: 500, enabled: true)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+        visit items_path
+
+        expect(page).to have_content(item_1.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/g/200/300']")
+        expect(page).to have_content(item_1.user.name)
+        expect(page).to have_content(item_1.inventory)
+        expect(page).to have_content(item_1.current_price)
+        expect(page).to_not have_content(item_2.name)
+        expect(page).to_not have_css("img[src*='https://picsum.photos/5472/3648?image=1083']")
+        expect(page).to_not have_content(item_2.user.name)
+        expect(page).to_not have_content(item_2.inventory)
+        expect(page).to_not have_content(item_2.current_price)
+        expect(page).to have_content(item_3.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/200/300?image=0']")
+        expect(page).to have_content(item_3.user.name)
+        expect(page).to have_content(item_3.inventory)
+        expect(page).to have_content(item_3.current_price)
+      end
+    end
+    context 'as a non-registered visitor' do
+      it 'should see all enabled items and their information' do
+
+        user_2 = User.create(name: "user_1", password: "test", street: "street",
+          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
+        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+        inventory: 3, description: 'apple one', current_price: 200, enabled: true)
+        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        inventory: 4, description: 'apple two', current_price: 400, enabled: false)
+        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        inventory: 5, description: 'apple three', current_price: 500, enabled: true)
+
+        visit items_path
+
+        expect(page).to have_content(item_1.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/g/200/300']")
+        expect(page).to have_content(item_1.user.name)
+        expect(page).to have_content(item_1.inventory)
+        expect(page).to have_content(item_1.current_price)
+        expect(page).to_not have_content(item_2.name)
+        expect(page).to_not have_css("img[src*='https://picsum.photos/5472/3648?image=1083']")
+        expect(page).to_not have_content(item_2.user.name)
+        expect(page).to_not have_content(item_2.inventory)
+        expect(page).to_not have_content(item_2.current_price)
+        expect(page).to have_content(item_3.name)
+        expect(page).to have_css("img[src*='https://picsum.photos/200/300?image=0']")
+        expect(page).to have_content(item_3.user.name)
+        expect(page).to have_content(item_3.inventory)
+        expect(page).to have_content(item_3.current_price)
+      end
+    end
+  end
+end
--- spec/features/user_show_page_spec.rb
+++ spec/features/user_show_page_spec.rb
@@ -21,19 +21,19 @@ describe 'USER SHOW PAGE' do
 
       expect(page).to_not have_content(user_1.password)
     end
-    
+
     it 'allows me to go to a form to edit my data that has all my data pre-entered' do
       user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1',
         zip: 'ZIP1', email: 'email1@aol.com', password: 'password1')
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      
+
       visit profile_path
-      
+
       click_link "Edit Profile"
-      
+
       expect(current_path).to eq(profile_edit_path)
-      
+
       expect(find_field("user[name]").value).to eq(user.name)
       expect(find_field("user[street]").value).to eq(user.street)
       expect(find_field("user[city]").value).to eq(user.city)
@@ -47,16 +47,16 @@ describe 'USER SHOW PAGE' do
       old_city = 'City One'
       user = User.create(name: old_name, street: 'Street One', city: old_city, state: 'State1',
         zip: 'ZIP1', email: 'email1@aol.com', password: 'password1')
-        
+
         new_name = 'NEW NAME'
         new_city = 'NEW CITY'
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      
+
       visit profile_edit_path
-      
+
       fill_in :user_name, with: new_name
       fill_in :user_city, with: new_city
-      
+
       click_button 'Submit'
       expect(current_path).to eq(profile_path)
       expect(page).to have_content('You have updated your profile')
@@ -78,18 +78,18 @@ describe 'USER SHOW PAGE' do
       old_email = 'email1@aol.com'
       user = User.create(name: old_name, street: old_street, city: old_city, state: old_state,
         zip: old_zip, email: old_email, password: 'password1')
-        
+
       new_name = 'NEW NAME'
       new_street = 'New Street'
       new_city = 'NEW CITY'
       new_state = 'New State'
       new_zip = 'zip2'
       new_email = 'email2@aol.com'
-        
+
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      
+
       visit profile_edit_path
-      
+
       fill_in :user_name, with: new_name
       fill_in :user_street, with: new_street
       fill_in :user_city, with: new_city
@@ -97,7 +97,7 @@ describe 'USER SHOW PAGE' do
       fill_in :user_zip, with: new_zip
       fill_in :user_email, with: new_email
       click_button 'Submit'
-      
+
       expect(current_path).to eq(profile_path)
       expect(page).to have_content('You have updated your profile')
       expect(page).to have_content(new_name)
@@ -117,12 +117,12 @@ describe 'USER SHOW PAGE' do
       user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1',
         zip: 'ZIP1', email: 'email1@aol.com', password: 'password1')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      
+
       visit profile_edit_path
-      
+
       fill_in :user_name, with: ''
       click_button 'Submit'
-      
+
       expect(current_path).to eq(profile_edit_path)
       expect(page).to have_content('Required fields are missing')
       expect(find_field("user[name]").value).to eq(user.name)
