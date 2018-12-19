615e67d33f45a9c4ed7243dbf82e7fc851cd64b9
Brad Niedt <bradleyniedt@gmail.com>
2018-12-19 16:21:28 -0700

    Fixed repeated emails for user objects in item index test

--- spec/features/item_index_page_spec.rb
+++ spec/features/item_index_page_spec.rb
@@ -5,21 +5,21 @@ describe 'as a visitor' do
     context 'as a default user' do
       it 'should see all enabled items and their information' do
         user_1 = User.create(name: "user_1", password: "test", street: "street",
-          city: "city", state: "CO", zip: "80219", email: "email", role: 0, enabled: true)
+          city: "city", state: "CO", zip: "80219", email: "email1@aol.com", role: 0)
         user_2 = User.create(name: "user_1", password: "test", street: "street",
-          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
-        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+          city: "city", state: "CO", zip: "80219", email: "email2@aol.com", role: 1)
+        item_1 = user_2.items.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
         inventory: 3, description: 'apple one', current_price: 200, enabled: true)
-        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        item_2 = user_2.items.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
         inventory: 4, description: 'apple two', current_price: 400, enabled: false)
-        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        item_3 = user_2.items.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
         inventory: 5, description: 'apple three', current_price: 500, enabled: true)
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
         visit items_path
 
-        within("#item-#{item_1.id}") do 
+        within("#item-#{item_1.id}") do
           expect(page).to have_content(item_1.name)
           expect(page).to have_link("#{item_1.name}", item_path(item_1))
           expect(page).to have_css("img[src*='https://picsum.photos/g/200/300']")
@@ -54,14 +54,14 @@ describe 'as a visitor' do
     context 'as a merchant user' do
       it 'should see all enabled items and their information' do
         user_1 = User.create(name: "user_1", password: "test", street: "street",
-          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
+          city: "city", state: "CO", zip: "80219", email: "email1@aol.com", role: 1)
         user_2 = User.create(name: "user_1", password: "test", street: "street",
-          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
-        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+          city: "city", state: "CO", zip: "80219", email: "email2@aol.com", role: 1)
+        item_1 = user_2.items.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
         inventory: 3, description: 'apple one', current_price: 200, enabled: true)
-        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        item_2 = user_2.items.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
         inventory: 4, description: 'apple two', current_price: 400, enabled: false)
-        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        item_3 = user_2.items.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
         inventory: 5, description: 'apple three', current_price: 500, enabled: true)
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
@@ -103,14 +103,14 @@ describe 'as a visitor' do
     context 'as an admin user' do
       it 'should see all enabled items and their information' do
         user_1 = User.create(name: "user_1", password: "test", street: "street",
-          city: "city", state: "CO", zip: "80219", email: "email", role: 2, enabled: true)
+          city: "city", state: "CO", zip: "80219", email: "email1@aol.com", role: 2)
         user_2 = User.create(name: "user_1", password: "test", street: "street",
-          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
-        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+          city: "city", state: "CO", zip: "80219", email: "email2@aol.com", role: 1)
+        item_1 = user_2.items.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
         inventory: 3, description: 'apple one', current_price: 200, enabled: true)
-        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        item_2 = user_2.items.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
         inventory: 4, description: 'apple two', current_price: 400, enabled: false)
-        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        item_3 = user_2.items.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
         inventory: 5, description: 'apple three', current_price: 500, enabled: true)
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
@@ -153,12 +153,12 @@ describe 'as a visitor' do
       it 'should see all enabled items and their information' do
 
         user_2 = User.create(name: "user_1", password: "test", street: "street",
-          city: "city", state: "CO", zip: "80219", email: "email", role: 1, enabled: true)
-        item_1 = user_2.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
+          city: "city", state: "CO", zip: "80219", email: "email2@aol.com", role: 1)
+        item_1 = user_2.items.create(name: 'apple1', image_link: 'https://picsum.photos/g/200/300',
         inventory: 3, description: 'apple one', current_price: 200, enabled: true)
-        item_2 = user_2.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
+        item_2 = user_2.items.create(name: 'apple2', image_link: 'https://picsum.photos/5472/3648?image=1083',
         inventory: 4, description: 'apple two', current_price: 400, enabled: false)
-        item_3 = user_2.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
+        item_3 = user_2.items.create(name: 'apple3', image_link: 'https://picsum.photos/200/300?image=0',
         inventory: 5, description: 'apple three', current_price: 500, enabled: true)
 
         visit items_path
