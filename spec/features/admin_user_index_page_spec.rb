8fbe8c37be8d366c13f1223fa953f5c8802c02f4
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-20 12:50:56 -0700

    Adds merge fixes

---
+++ spec/features/admin_user_index_page_spec.rb
@@ -0,0 +1,49 @@
+
+require 'rails_helper'
+
+describe 'ADMIN USER INDEX PAGE' do
+  context 'As an Admin user, when I click the Users nav link' do
+    it 'shows a list of user information and associated links' do
+      #Defaul Users
+      user_1 = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1',
+      zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 0, enabled: true)
+      user_2 = User.create(name: 'User Two', street: 'Street Two', city: 'City Two', state: 'State2',
+      zip: 'ZIP2', email: 'email2@aol.com', password: 'password2', role: 0, enabled: true)
+      user_3 = User.create(name: 'User Three', street: 'Street Three', city: 'City Three', state: 'State3',
+      zip: 'ZIP3', email: 'email3@aol.com', password: 'password3', role: 0, enabled: false)
+      user_4 = User.create(name: 'User Four', street: 'Street Four', city: 'City Four', state: 'State4',
+      zip: 'ZIP4', email: 'email4@aol.com', password: 'password4', role: 0, enabled: true)
+      #Merchant User
+      user_5 = User.create(name: 'User Five', street: 'Street Five', city: 'City Five', state: 'State5',
+      zip: 'ZIP5', email: 'email5@aol.com', password: 'password5', role: 1, enabled: true)
+      #Admin User
+      user_6 = User.create(name: 'User Six', street: 'Street Six', city: 'City Six', state: 'State6',
+      zip: 'ZIP6', email: 'email6@aol.com', password: 'password6', role: 2, enabled: true)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_6)
+
+      visit admin_users_path
+
+      within "#user-links-#{user_1.id}" do
+        expect(page).to have_link(user_1.name)
+        expect(page).to have_content(user_1.created_at)
+        expect(page).to have_button("Disable")
+      end
+      within "#user-links-#{user_2.id}" do
+        expect(page).to have_link(user_2.name)
+        expect(page).to have_content(user_1.created_at)
+        expect(page).to have_button("Disable")
+      end
+      within "#user-links-#{user_3.id}" do
+        expect(page).to have_link(user_3.name)
+        expect(page).to have_content(user_1.created_at)
+        expect(page).to have_button("Enable")
+      end
+      within "#user-links-#{user_4.id}" do
+        expect(page).to have_link(user_4.name)
+        expect(page).to have_content(user_1.created_at)
+        expect(page).to have_button("Disable")
+      end
+    end
+  end
+end
