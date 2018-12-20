8d9b01f1bfcead19bed1f1eb9db5b9682ef34f4f
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-19 17:35:15 -0700

    Adds filtering to new controllers

--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -1,5 +1,15 @@
 class Profile::OrdersController < ApplicationController
+    before_action :require_default_user
+    
     def index
       
+    end
+    
+    private
+    
+    def require_default_user
+      unless current_default?
+        render file: "public/404", status: 404, layout: false
+      end
     end
 end
--- app/controllers/profile/users_controller.rb
+++ app/controllers/profile/users_controller.rb
@@ -1,10 +1,18 @@
 class Profile::UsersController < ApplicationController
-  
+  before_action :require_default_user
   def show
     
   end
   
   def edit
     @user = current_user
+  end
+  
+  private
+  
+  def require_default_user
+    unless current_default?
+      render file: "public/404", status: 404, layout: false
+    end
   end
 end
