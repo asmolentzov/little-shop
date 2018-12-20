28b748d203f077f2751c5fe4cb0c7402e971ff76
William Petty <william@snakeweed.com>
2018-12-20 13:58:22 -0700

    Add item to cart method working

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,13 @@
 class Cart
-  
+  attr_reader :contents
+
+  def initialize(contents)
+    @contents = contents
+  end
+
+  def add_item(item_id)
+    @contents[item_id.to_s] ||= 0
+    @contents[item_id.to_s] += 1
+  end
 
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Cart do
   describe 'instance methods' do
     describe '#add_item' do
       it 'should add items to cart' do
-        cart = Cart.new()
+        cart = Cart.new({})
         cart.add_item(1)
         expect(cart.contents).to eq({'1' => 1})
         cart.add_item(2)
