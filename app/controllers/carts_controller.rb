1f1778918bb03ecc32d69a71a009c3d0e17ceea6
William Petty <william@snakeweed.com>
2018-12-20 14:52:04 -0700

    Adding create method to cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,13 @@ class CartsController < ApplicationController
     @cart = Cart.new(session[:cart])
   end
 
+  def create
+    item = Item.find(params[:item_id])
+    item_id_str = item.id.to_s
+    session[:cart] ||= Hash.new(0)
+    session[:cart][item_id_str] ||= 0
+    session[:cart][item_id_str] += 1
+    redirect_to items_path
+  end
+
 end
