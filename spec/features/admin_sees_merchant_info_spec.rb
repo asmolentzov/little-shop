8c46a1d776c1b4a6cb802c0dcca903bd0b2d5321
Daniel Briechle <danbriechle@hotmail.com>
2018-12-20 19:08:37 -0700

    creates test for admin merchanats show path

--- spec/features/admin_sees_merchant_info_spec.rb
+++ spec/features/admin_sees_merchant_info_spec.rb
@@ -45,5 +45,34 @@ describe 'As an admin' do
     expect(page).to_not have_link(admin_merchants_path(merchant_1.id))
     end
 
+    it 'can navigate to indivdual merchant paths' do
+
+      merchant_1 = User.create(name: 'Argellica Jones', street: '9 Slider Ave', city: 'Smithtown', state: 'PA',
+        zip: '76390', email: 'Jonesey@aol.com', password: '123456789', role: 1, enabled: true)
+      merchant_2 = User.create(name: 'Holden Butts', street: '5607 E County Rd.', city: 'bifftown', state: 'CO',
+          zip: '21154', email: 'Butts1045@aol.com', password: 'abc123', role: 1, enabled: true)
+
+      admin = User.create(name: "user_1", password: "test", street: "street", city: "city", state: "CO", zip: "80219", email: "email", role: 2, enabled: true)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit merchants_path
+
+      within "#merchant-#{merchant_1.id}" do
+        click_on admin_merchants_path(merchant_2.id)
+
+      expect(current_path).to eq(admin_merchants_path(merchant_2.id))
+      expect(page).to have_content(merchant_2.name)
+      expect(page).to have_content(merchant_2.city)
+      expect(page).to have_content(merchant_2.state)
+
+      expect(page).to_not have_content(merchant_2.name)
+      expect(page).to_not have_content(merchant_2.city)
+      expect(page).to_not have_content(merchant_2.state)
+      end
+
+
+    end
+
   end
 end
