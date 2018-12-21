3c0c97f490297ce6b1aca11d04dd372b1ede8412
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-20 19:53:54 -0700

    Adds item factory

--- spec/factories/factory_bot.rb
+++ spec/factories/factory_bot.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@aol.com" }
     sequence(:password) { |n| "password-#{n}" }
     sequence(:name) { |n| "User #{n}" }
-    sequence(:address) { |n| "Address #{n}" }
+    sequence(:street) { |n| "Address #{n}" }
     sequence(:city) { |n| "city #{n}" }
     sequence(:state) { |n| "state #{n}" }
     sequence(:zip) { |n| "zip #{n}" }
@@ -13,7 +13,7 @@ FactoryBot.define do
     sequence(:email) { |n| "merchant_#{n}@aol.com" }
     sequence(:password) { |n| "password-#{n}" }
     sequence(:name) { |n| "Merchant #{n}" }
-    sequence(:address) { |n| "Address #{n}" }
+    sequence(:street) { |n| "Address #{n}" }
     sequence(:city) { |n| "city #{n}" }
     sequence(:state) { |n| "state #{n}" }
     sequence(:zip) { |n| "zip #{n}" }
@@ -24,10 +24,20 @@ FactoryBot.define do
     sequence(:email) { |n| "admin_#{n}@aol.com" }
     sequence(:password) { |n| "password-#{n}" }
     sequence(:name) { |n| "Admin #{n}" }
-    sequence(:address) { |n| "Address #{n}" }
+    sequence(:street) { |n| "Address #{n}" }
     sequence(:city) { |n| "city #{n}" }
     sequence(:state) { |n| "state #{n}" }
     sequence(:zip) { |n| "zip #{n}" }
     role { 2 }
+  end
+  
+  factory :item do
+    sequence(:name) { |n| "Item #{n}" }
+    sequence(:image_link) { |n| "#{n}.jpg" }
+    sequence(:inventory) { |n| n }
+    sequence(:description) { |n| "A lovely example of an item#{n}" }
+    sequence(:current_price) { |n| n * 100 }
+    enabled { true }
+    merchant 
   end
 end
