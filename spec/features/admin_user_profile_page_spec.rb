233feb2410ad1bfa7068c7778c370448569b7b4d
Brad Niedt <bradleyniedt@gmail.com>
2018-12-22 12:28:58 -0700

    Adds test to admin user profile spec to test for visiting merchant user profile.

--- spec/features/admin_user_profile_page_spec.rb
+++ spec/features/admin_user_profile_page_spec.rb
@@ -22,4 +22,26 @@ describe 'As an admin user' do
       expect(page).to_not have_content(user_1.password)
     end
   end
+
+  describe  'when I visit a merchant user profile page' do
+    it 'should see the same information that a user sees' do
+      user_1 = create(:merchant)
+      user_2 = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_2)
+
+      visit admin_user_path(user_1)
+
+      expect(page).to have_content(user_1.name)
+      expect(page).to have_content(user_1.street)
+      expect(page).to have_content(user_1.city)
+      expect(page).to have_content(user_1.state)
+      expect(page).to have_content(user_1.zip)
+      expect(page).to have_content(user_1.email)
+
+      expect(page).to have_link('My Items')
+
+      expect(page).to_not have_content(user_1.password)
+    end
+  end
 end
