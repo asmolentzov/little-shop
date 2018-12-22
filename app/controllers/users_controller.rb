658a77bb48d9eb9853ee33e8f3738c58940c6d2d
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-22 14:29:56 -0700

    Adds slowest merchants to view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     @top_merchants_by_quantity = User.merchants_by_quantity
     @top_merchants_by_price = User.merchants_by_price
     @top_merchants_by_time = User.merchants_by_time[0..2]
+    @bottom_merchants_by_time = User.merchants_by_time.reverse[0..2]
   end
 
   def show
