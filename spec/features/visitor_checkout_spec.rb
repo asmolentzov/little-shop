ec0f93799f4ceb301b0333c22e75927ba351560a
Brad Niedt <bradleyniedt@gmail.com>
2018-12-22 16:35:20 -0700

    Creates test for unregistered user seeing register or login link and message when viewing cart

---
+++ spec/features/visitor_checkout_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'when I add items to my cart and I visit my cart page' do
+    it 'should see a message telling me to register or login to checkout' do
+      item_1 = create(:item)
+      item_2 = create(:item)
+
+      visit items_path
+
+      within("#item-#{item_1.id}") do
+        click_button 'Add Item'
+      end
+
+      visit cart_path
+
+      expect(page).to have_content('You must register or log in to checkout')
+      expect(page).to have_link('register')
+      expect(page).to have_link('log in')
+    end
+  end
+end
