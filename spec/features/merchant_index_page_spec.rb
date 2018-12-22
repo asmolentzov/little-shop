d2d7bdd5007887f62aefcecdaad8d7c7abe605f6
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-22 12:19:34 -0700

    Adds feature test for top merchants by quantity

--- spec/features/merchant_index_page_spec.rb
+++ spec/features/merchant_index_page_spec.rb
@@ -27,5 +27,36 @@ describe 'as a visitor' do
       expect(page).to have_content(user_3.state)
       expect(page).to have_content(user_3.created_at)
     end
+    
+    it 'should show an area with statistics showing top merchants by price and quantity' do
+      merchant_1 = create(:merchant)
+      item_1 = create(:item, user: merchant_1)
+      item_2 = create(:item, user: merchant_1)
+      item_3 = create(:item, user: merchant_1)
+      create(:fulfilled_order_item, item: item_1)
+      create(:fulfilled_order_item, item: item_2)
+      create(:fulfilled_order_item, item: item_3)
+      
+      merchant_2 = create(:merchant)
+      item_6 = create(:item, user: merchant_2)
+      create(:fulfilled_order_item, item: item_6)
+      
+      merchant_3 = create(:merchant)
+      
+      merchant_4 = create(:merchant)
+      item_4 = create(:item, user: merchant_4)
+      item_5 = create(:item, user: merchant_4)
+      create(:fulfilled_order_item, item: item_4)
+      create(:fulfilled_order_item, item: item_5)
+      
+      # within "#statistics" do
+      #   expect(page).to have_content("Top Merchants by Price: ")
+      # end
+      # 
+      within "#statistics" do
+        expect(page).to have_content("Top Merchants by Quantity: \n#{merchant_1.name}\n#{merchant_4.name}\n#{merchant_2.name}")
+      end
+      
+    end
   end
 end
