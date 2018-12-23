81e1db4f77a2b8954c810bf25654790a5be0aefc
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-22 17:11:28 -0700

    Adds initial biggest orders method

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,12 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   enum status: [:pending, :fulfilled, :cancelled]
+  
+  def self.biggest_orders
+    joins(:order_items)
+    .select("orders.*, count(order_items.id) AS item_count")
+    .group(:id)
+    .order("item_count DESC")
+    .limit(3)
+  end
 end
