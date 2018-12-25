95f28ebb416a2271a8be1c023a7ee751b784cbf3
Brad Niedt <bradleyniedt@gmail.com>
2018-12-24 18:16:39 -0700

    Adds revisions per github comments. Dryed up the item controller top five and bottom five instance variables and fixed test variable names.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,13 +3,11 @@ class ItemsController < ApplicationController
   def index
    if current_merchant?
      @items = current_user.items
-     @top_five_items = Item.five_popular('desc')
-     @bottom_five_items = Item.five_popular('asc')
    else
      @items = Item.all
-     @top_five_items = Item.five_popular('desc')
-     @bottom_five_items = Item.five_popular('asc')
    end
+   @top_five_items = Item.five_popular('desc')
+   @bottom_five_items = Item.five_popular('asc')
   end
 
   def show
