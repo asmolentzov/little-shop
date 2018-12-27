87426843479538261cae2a795471ff6814c4b960
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-27 16:53:26 -0700

    Adds feature test for orders displaying on merchant dashboard page

--- spec/features/merchant_show_page_spec.rb
+++ spec/features/merchant_show_page_spec.rb
@@ -18,5 +18,27 @@ describe 'as a merchant user' do
       expect(page).to have_link('My Items')
       expect(page).to_not have_link('Edit Profile')
     end
+    
+    it 'should show me a list of pending orders containing my items' do
+      merchant = create(:merchant)
+      allow_any_instance_of(ApplicationController).to recieve(current_user).and_return(merchant)
+      
+      order_1 = create(:order)
+      item_1 = create(:unfulfilled_order_item, order: order_1)
+      item_2 = create(:unfulfilled_order_item, order: order_1)      
+      
+      visit dashboard_path
+      
+      within "#pending-order-#{order_1.id}" do
+        expect(page).to have_link("Order ##{order_1.id}")
+        expect(page).to have_content("Placed on: #{order_1.created_at}")
+        expect(page).to have_content("My items in order: #{order_1.my_items_quantity}")
+        expect(page).to have_content("My items value: #{order_1.my_items_value}")
+        click_link "Order ##{order_1.id}"
+      end
+      
+      expect(current_path).to eq(dashboard_orders_path(order_1))
+      
+    end
   end
 end
