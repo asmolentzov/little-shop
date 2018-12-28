23457a556203b5f157fd3f2d5def0b2d7d687656
William Petty <william@snakeweed.com>
2018-12-28 17:50:59 +0000

    Test and view code to make thumbnail work

--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -1,7 +1,11 @@
 class Profile::OrdersController < ApplicationController
   before_action :require_default_user
-  
+
   def index
-    
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
 end
--- spec/features/user_order_show_page_spec.rb
+++ spec/features/user_order_show_page_spec.rb
@@ -27,15 +27,14 @@ describe 'USER ORDER SHOW PAGE' do
       within "#order-item-#{order_item_1.id}" do
         expect(page).to have_content("Item: #{order_item_1.item.name}")
         expect(page).to have_content("Description: #{order_item_1.item.description}")
-        expect(page).to have_content("Thumbnail: #{order_item_1.item.image_link}")
+        expect(page).to have_css("img[src*='#{order_item_1.item.image_link}']")
         expect(page).to have_content("Quantity: #{order_item_1.quantity}")
-        expect(page).to have_content("Quantity: #{order_item_1.order_price}")
+        expect(page).to have_content("Order Price: #{order_item_1.order_price}")
         expect(page).to have_content("Subtotal: #{order_item_1.order_price * order_item_1.quantity}")
       end
 
-      expect(page).to have_content("Total items in order: 1")
-      expect(page).to have_content("Grand total: $1.00")
-
+      expect(page).to have_content("Total items in order: 6")
+      expect(page).to have_content("Grand total: $14.00")
     end
   end
 end
