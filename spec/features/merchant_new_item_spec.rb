2bb2b187c043a46e9030877c95a26ba16ab2340f
Brad Niedt <bradleyniedt@gmail.com>
2018-12-28 18:25:54 -0700

    Edits test for creating item with negative inventory

--- spec/features/merchant_new_item_spec.rb
+++ spec/features/merchant_new_item_spec.rb
@@ -5,7 +5,10 @@ describe 'as a merchant user' do
     it 'should see a link to add a new item. when I click it, I am taken to a new item page with a form where I can create a new item' do
       merch = create(:merchant)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merch)
+      visit login_path
+      fill_in :email, with: merch.email
+      fill_in :password, with: merch.password
+      click_button 'Log In'
 
       visit dashboard_items_path
       expect(page).to have_link('Add New Item')
@@ -59,10 +62,13 @@ describe 'as a merchant user' do
       expect(current_path).to eq('/dashboard/items/new')
       expect(page).to have_content("Current price can't be blank")
     end
-    it 'I can create a new item without an image and a placeholder image will display' do
+    it 'I cannot create an item with inventory less than 0' do
       merch = create(:merchant)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merch)
+      visit login_path
+      fill_in :email, with: merch.email
+      fill_in :password, with: merch.password
+      click_button 'Log In'
 
       visit dashboard_items_path
 
@@ -75,25 +81,11 @@ describe 'as a merchant user' do
       fill_in :item_name, with: 'item_1'
       fill_in :item_description, with: 'this is a great item_1'
       fill_in :item_current_price, with: 3000
-      fill_in :item_inventory, with: 1
+      fill_in :item_inventory, with: -1
       click_on('Create Item')
 
-      expect(current_path).to eq(dashboard_items_path)
-      expect(page).to have_content('Your new item has been created')
-
-      item_1 = Item.all.first
-
-      within("#item-#{item_1.id}") do
-        expect(page).to have_content(item_1.id)
-        expect(page).to have_content(item_1.name)
-        expect(page).to have_css("img[src*='https://picsum.photos/200/300?image=0']")
-        expect(page).to have_content(item_1.current_price)
-        expect(page).to have_content(item_1.inventory)
-        expect(page).to have_link('Edit this item')
-        expect(page).to have_link('Disable this item')
-        expect(page).to have_link('Delete this item')
-        expect(page).to_not have_link('Enable this item')
-      end
+      expect(current_path).to eq('/dashboard/items/new')
+      expect(page).to have_content('Inventory is not included in the list')
     end
   end
 end
