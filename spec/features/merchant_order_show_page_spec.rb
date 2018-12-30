7e9dc305a501f096b712ab47b1f0331e249d8f63
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-29 17:10:27 -0700

    Adds feature test for seeing item info on merchant order show page

--- spec/features/merchant_order_show_page_spec.rb
+++ spec/features/merchant_order_show_page_spec.rb
@@ -9,6 +9,7 @@ describe 'As a merchant' do
       item_2 = create(:item, user: merchant)
       
       item_3 = create(:item)
+      item_4 = create(:item, user: merchant)
       
       customer = create(:user)
       order = create(:order, user: customer)
@@ -26,9 +27,37 @@ describe 'As a merchant' do
       expect(page).to have_content(item_1.name)
       expect(page).to have_content(item_2.name)
       expect(page).to_not have_content(item_3.name)
+      expect(page).to_not have_content(item_4.name)
       expect(page).to_not have_css("#item-#{item_3.id}")
+      expect(page).to_not have_css("#item-#{item_4.id}")
       expect(page).to have_css("#item-#{item_1.id}")
       expect(page).to have_css("#item-#{item_2.id}")
+    end
+    
+    it 'should should information about my items on the page' do
+      merchant = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+      item_1 = create(:item, user: merchant)
+      item_2 = create(:item, user: merchant)
+      
+      order = create(:order)
+      oi_1 = create(:fulfilled_order_item, order: order, item: item_1)
+      oi_2 = create(:unfulfilled_order_item, order: order, item: item_2)
+      
+      visit dashboard_orders_path(order)
+      
+      within "#item-#{item_1.id}" do
+        expect(page).to have_link("#{item_1.name}")
+        expect(page).to have_css("img[src='#{item_1.image_link}']")
+        expect(page).to have_content("Price: #{item_1.current_price}")
+        expect(page).to have_content("Quantity: #{oi_1.quantity}")
+      end
+      within "#item-#{item_2.id}" do
+        expect(page).to have_link("#{item_2.name}")
+        expect(page).to have_css("img[src='#{item_2.image_link}']")
+        expect(page).to have_content("Price: #{item_2.current_price}")
+        expect(page).to have_content("Quantity: #{oi_2.quantity}")
+      end
     end
   end
 end
