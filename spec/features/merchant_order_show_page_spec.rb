f9deb69f03ad3b9ec52a2b9e5536eabb49c7d107
Brad Niedt <bradleyniedt@gmail.com>
2018-12-30 11:03:36 -0700

    Updates test for not fulfilling. Dan is working on the fulfill

--- spec/features/merchant_order_show_page_spec.rb
+++ spec/features/merchant_order_show_page_spec.rb
@@ -91,17 +91,23 @@ describe 'As a merchant' do
       user = create(:user)
       item_1 = create(:item, inventory: 3)
       item_2 = create(:item, inventory: 3)
-      item_3 = create(:item, inventory: 3)
       order_1 = create(:order, user: user)
       order_item_1 = create(:unfulfilled_order_item, item: item_1, quantity: 3, order: order_1)
       order_item_2 = create(:unfulfilled_order_item, item: item_2, quantity: 4, order: order_1)
-      order_item_3 = create(:unfulfilled_order_item, item: item_3, quantity: 1, order: order_1)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merch)
 
       visit dashboard_orders_path(order_1)
 
-      
+      within("item-#{item_2.id}") do
+        expect(page).to_not have_link('Fulfill')
+        expect(page).to have_content('Cannot Fulfill!')
+        expect(page).to have_css('p.cannot_fulfill')
+      end
+      within("item-#{item_1.id}") do
+        expect(page).to_not have_content('Cannot Fulfill!')
+        expect(page).to_not have_css('p.cannot_fulfill')
+      end
     end
   end
 end
