e0d3e94d81dd813da8a4cde5facb09a27d83ed93
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-29 21:54:40 -0700

    Adds requiring merchant user to dashboard users controller

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,4 +1,5 @@
 class Dashboard::UsersController < ApplicationController
+  before_action :require_merchant_user
   
   def show
     @merchant_pending_orders = Order.merchant_pending_orders(current_user.id)
