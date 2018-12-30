43d3e4902d4c344668f45a458329412007264ba1
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-29 21:11:32 -0700

    Updates items index page - merchant-specific in dashboard namespace, old one without merchant specifics

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -1,4 +1,13 @@
 class Dashboard::ItemsController < ApplicationController
   
+  def index
+   if current_merchant?
+     @items = current_user.items
+   else
+     @items = Item.enabled_items
+   end
+   @top_five_items = Item.five_popular('desc')
+   @bottom_five_items = Item.five_popular('asc')
+  end
   
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,6 @@
 class ItemsController < ApplicationController
 
-  def index
-   if current_merchant?
-     @items = current_user.items
-   else
-     @items = Item.enabled_items
-   end
-   @top_five_items = Item.five_popular('desc')
-   @bottom_five_items = Item.five_popular('asc')
-  end
+
 
   def show
     @item = Item.find(params[:id])
