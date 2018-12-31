b63325e5cdaa57a0dac2560e729302f4d15cf3e4
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-31 15:05:53 -0700

    Adds .includes to @items in controller to remove N + 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-   @items = Item.enabled_items
+   @items = Item.enabled_items.includes(:user)
    @top_five_items = Item.five_popular('desc')
    @bottom_five_items = Item.five_popular('asc')
   end
