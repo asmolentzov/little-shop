ebd3c4f26af7f960c9b4cdf8afd4d45ddd8d35b0
Daniel Briechle <danbriechle@hotmail.com>
2018-12-30 17:19:44 -0700

    adds lenght validator to zip code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:city)}
     it { should validate_presence_of(:state)}
     it { should validate_presence_of(:zip)}
+    it { should validate_length_of(:zip).is_at_most(5) }
+    it { should validate_length_of(:zip).is_at_least(5) }
     it { should validate_presence_of(:email)}
     it { should validate_uniqueness_of(:email)}
     it { should validate_presence_of(:password)}
