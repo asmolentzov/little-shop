33fd7b9d3413fb822d8730208508e3a4e02ef609
Daniel Briechle <danbriechle@hotmail.com>
2018-12-30 17:16:18 -0700

    changes all zip codes to 5 digits

--- spec/features/user_can_logout_spec.rb
+++ spec/features/user_can_logout_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'as a logged in user' do
   context 'default user' do
     it 'should be able to log out' do
-      user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1')
+      user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: '12343', email: 'email1@aol.com', password: 'password1')
 
       visit login_path
 
@@ -59,7 +59,7 @@ describe 'as a logged in user' do
   end
   context 'merchant user' do
     it 'should be able to log out' do
-      user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 1)
+      user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: '12345', email: 'email1@aol.com', password: 'password1', role: 1)
 
       visit login_path
 
@@ -78,7 +78,7 @@ describe 'as a logged in user' do
   end
   context 'admin user' do
     it 'should be able to log out' do
-      user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: 'ZIP1', email: 'email1@aol.com', password: 'password1', role: 2)
+      user = User.create(name: 'User One', street: 'Street One', city: 'City One', state: 'State1', zip: '12345', email: 'email1@aol.com', password: 'password1', role: 2)
 
       visit login_path
 
