2df38fb8e4b4b61bb6e44bac6806ab5f08e2588e
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-30 20:12:05 -0700

    Adds feature tests for editing an item sad path

--- spec/features/admin_manages_merchant_orders_spec.rb
+++ spec/features/admin_manages_merchant_orders_spec.rb
@@ -140,5 +140,71 @@ describe 'As an admin user' do
       end
       expect(Item.find(@item.id).enabled).to eq(true)
     end
+    
+    it 'will have certain restrictions for editing information' do
+      visit edit_admin_merchant_item_path(@merchant, @item)
+      
+      fill_in :item_name, with: nil
+      click_button 'Update Item'
+      
+      expect(page).to have_content("Name can't be blank")
+      expect(@item.name).to_not eq(nil)
+      expect(find_field("item[name]").value).to eq(@item.name)
+      
+      fill_in :item_description, with: nil
+      click_button 'Update Item'
+      expect(page).to have_content("Description can't be blank")
+      expect(@item.description).to_not eq(nil)
+      expect(find_field("item[description]").value).to eq(@item.description)
+      
+      fill_in :item_current_price, with: -200
+      click_button 'Update Item'
+      expect(page).to have_content("Current price must be greater than or equal to 0")
+      expect(@item.current_price).to_not eq(-200)
+      expect(find_field("item[current_price]").value).to eq(@item.current_price.to_s)
+      
+      fill_in :item_inventory, with: 0
+      click_button 'Update Item'
+      expect(page).to have_content("Item ##{@item.id} has been updated")
+      
+      visit edit_admin_merchant_item_path(@merchant, @item)
+      @item = Item.find(@item.id)
+      
+      fill_in :item_inventory, with: -4
+      click_button 'Update Item'
+      expect(page).to have_content("Inventory must be greater than or equal to 0")
+      expect(@item.inventory).to_not eq(-4)
+      expect(find_field("item[inventory]").value).to eq(@item.inventory.to_s)
+    end
+    
+    it 'will let the image field be blank and replace with placeholder image' do
+      visit edit_admin_merchant_item_path(@merchant, @item)
+      
+      fill_in :item_image_link, with: nil
+      click_button 'Update Item'
+      expect(page).to have_content("Item ##{@item.id} has been updated")
+      expect(current_path).to eq(admin_merchant_items_path(@merchant))
+      within "#item-#{@item.id}" do
+        expect(page).to have_css("img[src*='https://picsum.photos/200/300?image=0']")
+      end
+    end
+    
+    it 'will keep an items disabled state when updating' do
+      disabled_item = create(:disabled_item, user: @merchant)
+      visit edit_admin_merchant_item_path(@merchant, disabled_item)
+      
+      new_name = "New!"
+      
+      fill_in :item_name, with: new_name
+      click_button 'Update Item'
+      
+      expect(page).to have_content("Item ##{disabled_item.id} has been updated")
+      expect(current_path).to eq(admin_merchant_items_path(@merchant))
+      within "#item-#{disabled_item.id}" do
+        expect(page).to have_content(new_name)
+        expect(page).to have_content('Enable this item')
+      end
+      expect(disabled_item.enabled).to eq(false)
+    end
   end
 end
